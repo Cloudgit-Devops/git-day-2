8f5b1b4c32b00c9330a15a5f72eac54554ffe65e
ayesha <ayeshask421@gmail.com>
2025-04-08 18:43:32 +0530

    app.py updated by developer-1

---
+++ app.py
@@ -0,0 +1,19 @@
+iV
+import boto3
+
+# Create an EC2 client
+ec2 = boto3.client('ec2')
+
+# Launch an EC2 instance
+response = ec2.run_instances(
+    ImageId='ami-0c55b159cbfafe1f0',  # Example Amazon Linux AMI ID, replace with your own
+    InstanceType='t2.micro',           # Instance type (t2.micro is eligible for free tier)
+    MinCount=1,                        # Minimum number of instances to launch
+    MaxCount=2,                        # Maximum number of instances to launch
+    KeyName='your-key-name',           # Replace with your EC2 key pair name
+)
+
+# Print the instance ID of the newly launched EC2 instance
+instance_id = response['Instances'][0]['InstanceId']
+print(f"EC2 instance {instance_id} launched successfully.")
+
